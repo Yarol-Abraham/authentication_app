c849a907c9fbbf0eeebee270cd6e90a2a5d716ee
Abraham <59816968+Yarol-Abraham@users.noreply.github.com>
2021-06-24 22:11:50 -0600

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 
+const dotenv = require('dotenv');
+dotenv.config({ path: "config.env" });
+
 const AppError = require('./utils/appError');
 const globalsErrors = require('./controllers/errorsController');
 
@@ -17,6 +20,13 @@ app.use(helmet() );
 
 // public assests - images/users
 app.use("/assets", express.static(path.resolve(__dirname, "assets")))
+
+//cors
+var corsOptions = {
+    origin: process.env.URL_FRONTEND,
+    optionsSuccessStatus: 200
+  }
+app.use(cors(corsOptions) );
 
 // Read data in format JSON
 app.use(express.json({ limit: '10kb' }) );
